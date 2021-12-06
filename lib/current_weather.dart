b3e940fe06c598279ac50449421cec9a6eb462e9
Tamima Chowdhury <92022949+tamimachy@users.noreply.github.com>
2021-12-06 10:37:22 +0600

    Weather App

--- lib/current_weather.dart
+++ lib/current_weather.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:weather_app/weather_provider.dart';
 
-class CurrentWeather extends StatelessWidget {
+class CurrentWeather extends StatefulWidget {
   const CurrentWeather({Key? key}) : super(key: key);
 
   @override
+  State<CurrentWeather> createState() => _CurrentWeatherState();
+}
+
+class _CurrentWeatherState extends State<CurrentWeather> {
+  bool isLoading = true;
+
+  void didChangeDependencies(){
+    Provider.of<WeatherProvider>(context).getCurrentWeatherInfo().then((value){
+      setState(() {
+        isLoading = false;
+      });
+    });
+    super.didChangeDependencies();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Current Weather"),
+    return Consumer<WeatherProvider>(
+        builder: (context, value, child) => isLoading?
+        Center(child: CircularProgressIndicator(),)
+            :Container(
+                height: 300,
+                width: 300,
+                decoration: BoxDecoration(
+                  color: Colors.cyan,
+                ),
+                child: Text("${value.getMainWeatherData.main.temp}"),
+            )
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:weather_app/model/weather_response.dart';
 import 'weather_provider.dart';
 import 'home_page.dart';
@@ -6,7 +7,14 @@ import 'home_page.dart';
 import 'weather_provider.dart';
 
 void main(){
-  runApp(MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (contex)=>WeatherProvider()),
+      ],
+      child: MyApp(),
+    )
+  );
 }
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
